3aa40bce45a950b677f54a8ec0588be089f50a96
Hioshiowo <89017324+Hioshiowo@users.noreply.github.com>
2021-10-16 22:37:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import random
 from replit import db
 from keep_alive import keep_alive
+from discord import Member
 
 client = discord.Client()
 
@@ -103,5 +104,35 @@ async def on_message(message):
                                "$about        $config       $inspire\r\n"
                                "$release      $github       $uwu\r\n"
                                "more in the Future!")
+
+  if message.content.startswith("!userinfo"):
+    args = message.content.split(" ")
+    if len(args) == 2:
+        member: Member = discord.utils.find(lambda m: args[1] in m.name, message.guild.members)
+        if member:
+          embed = discord.Embed(title="User info! {}".format(member.name),
+          description="User info of".format(member.mention),
+          color=0x22a7f0)
+          embed.add_field(name="Join date", value=member.joined_at.strftime("%d/%m/%Y, %H:%M:%S"), inline=True)
+
+          embed.add_field(name="Discord Join date", value=member.created_at.strftime("%d/%m/%Y, %H:%M:%S"), inline=True)
+          rollen = ""
+          for role in member.roles:
+            if not role.is_default():
+              rollen += "{} \r\n".format(role.mention)
+          if rollen:
+            embed.add_field(name="roles", value=rollen, inline=True)
+          embed.set_thumbnail(url=member.avatar_url)
+          embed.set_footer(text="Kawaii-bot ©TM®")
+          await message.channel.send(embed=embed)
+  if "!lov3" in message.content:
+    await message.channel.send("**I luv you Anni C: <3**")
+  if "-gamesense.jar" in message.content:
+    await message.channel.send("**Gamesense GitHub!:**\r\n"
+                                  "https://github.com/IUDevman/gamesense-client")        
+  
+
+
+
 keep_alive()
 client.run("You naught boy")
