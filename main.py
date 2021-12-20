55347d211951d88fdd21908dec116c4ea468f47b
Hioshiowo <89017324+Hioshiowo@users.noreply.github.com>
2021-12-20 21:54:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 from replit import db
 from keep_alive import keep_alive
 from discord import Member
+import second
 
 client = discord.Client()
 
@@ -127,12 +128,49 @@ async def on_message(message):
           await message.channel.send(embed=embed)
   if "!lov3" in message.content:
     await message.channel.send("**I luv you Anni C: <3**")
-  if "-gamesense.jar" in message.content:
+  if "EARTHHACK 3.0" in message.content:
     await message.channel.send("**Gamesense GitHub!:**\r\n"
-                                  "https://github.com/IUDevman/gamesense-client")        
+                                  "https://github.com/Gopro366")
+    r = requests.get('https://github.com/')
+    print(r.status_code)
+            
+  if "-cosmos.jar" in message.content:
+    await message.channel.send("**Cosmos GitHub!:**\r\n"
+                                "https://github.com/linustouchtips/cosmos")  
+  if "-rape" in message.content:
+    await message.channel.send("@everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone @everyone")
+  
+  if "-release date" in message.content:
+    await message.channel.send("**Kawaii Client Release Soon**\r\n"
+                                "#info! for the Info you fag!")
+  
+  if "sad" in message.content:
+    await message.channel.send("don't be sad.\r\n"
+                              "Be happy https://tenor.com/view/dog-smile-happy-good-boy-dog-smile-happy-good-boy-gif-21703225")
+    r = requests.get('https://google.com/')
+    print(r.status_code)
+  if "!setup" in message.content:
+    from discord_webhook import DiscordWebhook, DiscordEmbed
+
+    content = 'This is a Basic Command List what the Bot offers to you!'
+
+    webhook = DiscordWebhook(url='https://ptb.discord.com/api/webhooks/921940877344145408/_2krGoTSiAk3ay7fzqJ6VwpiofyELoxZ4mZX3S1gAQlZHV3FwabJvd1L5DApEMQcfxIW')
+
+    embed = DiscordEmbed(title='Kawaii-Bot is Currently in Beta', color=242424)
+    embed.set_author(name='Kawaii international Cheats-', url='https://www.nautiljon.com/images/perso/00/97/tohsaka_rin_11979.jpg', icon_url='https://www.nautiljon.com/images/perso/00/97/tohsaka_rin_11979.jpg')
+    embed.set_footer(text='Kawaii-bot International')
+    embed.set_timestamp()
+    embed.add_embed_field(name='Cheats release Date', value='Current cheats are free, but we are recommending EARTHHACK 3.0 because of the Many settings and the Good performance')
+    embed.set_image(url='https://www.nautiljon.com/images/perso/00/97/tohsaka_rin_11979.jpg')
+    webhook.add_embed(embed)
+    respose = webhook.execute()
+  
+
+  if "Zoomy" in message.content:
+    await message.channel.send("Zoomy is a Sussy Nigger!\r\n"
+                              "Everyone hits headshots exept for him")
   
 
 
-
 keep_alive()
-client.run("You naught boy")
+client.run("ODkyNzkzNTE5MzQ4MDE1MjA1.YVSEwg.-C2c4Z3RC3WGqW4jYsGi45mmxPo")
